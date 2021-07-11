d10dcb3ca44e6a0223669e13b51451892b6bee1e
jasjit <jaskaranminhas2017@gmail.com>
2021-07-12 01:10:44 +0400

    Firest-commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Fashion is Passion</a>
+      </header>
+      <main>Product List</main>
+      <footer>&copy; All Right is Reserved.</footer>
+    </div>
+  );
+}
+
+export default App;
